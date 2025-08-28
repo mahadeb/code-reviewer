60c9b37f30dd9ec17d356766f04a64bee1d0ecc6
Mahadeb Rajbangshi <mahadeb.rajbangshe@itconquest.com>
2025-08-28 23:47:34 +0600

    Ignore files and added example config

---
+++ example-config.py
@@ -0,0 +1,9 @@
+MODEL_PROVIDER = "gemini"  # Options: "openai" or "gemini"
+OPENAI_API_KEY = ""
+GEMINI_API_KEY = ""
+
+# --------------------------
+# GITLAB CONFIG
+# --------------------------
+GITLAB_API = ""
+GITLAB_TOKEN = ""
